85e916698c4ef0e420da8be804ab4eeeaf967b22
Leonard Sellam <leonard@honestica.com>
2016-11-14 00:32:40 +0100

    first commit create structure

---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG = True #Turns on debugging features on Flask (true in Dev false in prod)
+MAIL_FROM_EMAIL = "sellam.leonard@gmail.com" #For use in application email
+BCRYPT_LEVEL = 12
+SECRET_KEY = "dev key"
